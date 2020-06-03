e72639e048d27290dac8ba62762ebb5bd555171d
Francisco Thiago <ftathiago@gmail.com>
2020-06-03 08:46:06 -0300

    Refactor ApiController.GetResponse to accept status code

--- src/StackOverHead.Web/Controllers/ApiController.cs
+++ src/StackOverHead.Web/Controllers/ApiController.cs
@@ -1,15 +1,20 @@
+using System;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using StackOverHead.Web.Enums;
 using StackOverHead.Web.Models;
 
 namespace StackOverHead.Web.Controllers
 {
     public class ApiController : Controller
     {
-        protected IActionResult GetResponse<T>(T obj)
+        protected IActionResult GetResponse<T>(T obj, int statusCodes = StatusCodes.Status200OK)
         {
             var response = new ResponseDefault<T>();
             response.Data = obj;
-            return Ok(response);
+            var result = new ObjectResult(response);
+            result.StatusCode = statusCodes;
+            return result;
         }
     }
 }
