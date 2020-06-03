f912e22436446bb58c9156e548b5dd4f74330977
Francisco Thiago <ftathiago@gmail.com>
2020-06-03 08:48:41 -0300

    Add AnswerRequest DataAnnotations

--- src/StackOverHead.Question.App/Models/AnswerRequest.cs
+++ src/StackOverHead.Question.App/Models/AnswerRequest.cs
@@ -1,9 +1,11 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace StackOverHead.Question.App.Models
 {
     public class AnswerRequest
     {
+        [Required]
         public string Body { get; set; }
         public Guid UserId { get; set; }
     }
