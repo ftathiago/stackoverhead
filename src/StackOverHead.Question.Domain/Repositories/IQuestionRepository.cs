aa1c91c9a343c2b96b5f3c2b36231bdfbf2e7532
Francisco Thiago <ftathiago@gmail.com>
2020-06-04 10:37:25 -0300

    Update QuestionRepository to support RegisterComment

--- src/StackOverHead.Question.Domain/Repositories/IQuestionRepository.cs
+++ src/StackOverHead.Question.Domain/Repositories/IQuestionRepository.cs
@@ -1,7 +1,12 @@
+using System.Threading.Tasks;
 using StackOverHead.LibCommon.Repositories;
 using StackOverHead.Question.Domain.Entities;
 
 namespace StackOverHead.Question.Domain.Repositories
 {
-    public interface IQuestionRepository : IBaseRepository<QuestionEntity> { }
+    public interface IQuestionRepository : IBaseRepository<QuestionEntity>
+    {
+        Task RegisterCommentAsync(CommentEntity comment);
+    }
+
 }
