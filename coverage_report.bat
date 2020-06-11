07aec647e1332b1a34e9e3e4accea173b07273fd
Francisco Thiago <ftathiago@gmail.com>
2020-06-11 13:08:51 -0300

    Add code coverage report generation

--- coverage_report.bat
+++ coverage_report.bat
@@ -1,4 +1,5 @@
 dotnet tool install -g dotnet-reportgenerator-globaltool
-dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput=./coverage.opencover.xml
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput=coverage.opencover.xml
 reportgenerator -reports:**/coverage.opencover.xml -targetdir:coverage_report
+# Abre o browser padrão
 coverage_report\index.html
