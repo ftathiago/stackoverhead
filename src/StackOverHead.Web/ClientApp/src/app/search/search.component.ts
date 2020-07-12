f75359c9e60d575a2ff0a4f7c66f01e53cce7715
Francisco Thiago <ftathiago@gmail.com>
2020-07-12 11:29:50 -0300

    Apply template to boilerplate

---
+++ src/StackOverHead.Web/ClientApp/src/app/search/search.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent {
+  isExpanded = false;
+
+  collapse() {
+    this.isExpanded = false;
+  }
+
+  toggle() {
+    this.isExpanded = !this.isExpanded;
+  }
+}
